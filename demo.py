abad3dd38607fa3649c983de7bd8ecc96c0f10a9
Mahesh Kumar <2184mk@gmail.com>
2026-02-06 14:53:37 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,13 @@
+"""
+This module contains utility functions for demo purposes.
+"""
 def my_function():
     # password = "test@123"
     # b = 6
     # c = 6
     # return d+password
+    """
+    This module contains utility functions for demo purposes.
+    """
     return "mahesh demo"
 my_function()
