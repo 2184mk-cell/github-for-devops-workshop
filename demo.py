4ec7450f611aca042c33efae687880ac5fa473ea
Mahesh Kumar <2184mk@gmail.com>
2026-02-06 14:50:35 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 def my_function():
-    password = "test@123"
-    b = 6
-    c = 6
-    return d+password
+    # password = "test@123"
+    # b = 6
+    # c = 6
+    # return d+password
+    return "mahesh demo"
 my_function()
